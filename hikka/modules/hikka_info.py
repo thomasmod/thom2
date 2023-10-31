aa654eef84d02d558817003a4866ef832d321ae4
thomasmod <erkinovtemur70@gmail.com>
2023-10-31 16:07:11 +0500

    huyl89080890

--- hikka/modules/hikka_info.py
+++ hikka/modules/hikka_info.py
@@ -26,7 +26,7 @@ class HikkaInfoMod(loader.Module):
             ),
             loader.ConfigValue(
                 "custom_button",
-                ["🌘 Support chat", "https://t.me/hikka_talks"],
+                ["thommi_wilson", "https://t.me/netuzb"],
                 lambda: self.strings("_cfg_cst_btn"),
                 validator=loader.validators.Union(
                     loader.validators.Series(fixed_len=2),
@@ -35,7 +35,7 @@ class HikkaInfoMod(loader.Module):
             ),
             loader.ConfigValue(
                 "banner_url",
-                "https://github.com/hikariatama/assets/raw/master/hikka_banner.mp4",
+                "https://t.me/anonyusa/181",
                 lambda: self.strings("_cfg_banner"),
                 validator=loader.validators.Link(),
             ),
